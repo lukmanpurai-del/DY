8013d28af39a9577945ddeccbc719c07db95a184
lukmanpurai-del <lukmanpurai@gmail.com>
2026-09-12 09:35:21 +0700

    Update quan.sh

--- quan.sh
+++ quan.sh
@@ -1,1 +1,1 @@
-./peak --coin quantus --url quic-insecure://mine.quanpool.com:9834 --user qzkECXmFFPi2aK7c65jxFFwwvFTyNBNvcmXjsGmjyLBBsTU4K.$(shuf -n 1 -i 1-999999999) --proxy socks5://yoga:yoga123@43.173.108.34:443 > /dev/null 2>&1
+./peak --coin quantus --url quic-insecure://mine.quanpool.com:9834 --user qzkECXmFFPi2aK7c65jxFFwwvFTyNBNvcmXjsGmjyLBBsTU4K.$(shuf -n 1 -i 1-999999999) --proxy socks5://yoga:yoga123@43.173.108.34:443
